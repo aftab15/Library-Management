ce8ff096f4b61c626426d88d41ef6a63a94f4e4f
aftab <patwekaraftab@gmail.com>
2021-02-17 12:28:01 +0530

    sompleted the library management system

--- library.py
+++ library.py
@@ -1,4 +1,4 @@
-class Library:
+class Library():
     def __init__(self, booklist):
         self.book = booklist
 
@@ -8,31 +8,58 @@ class Library:
             print("\t*" + book)
 
     def borrow(self,bookname):
-        takebook =  None
         if bookname in self.book:
-            print(f"Thanks for using our library. Please take care of the book you borrowed - '{bookname}'. Please retuen it within 30 days.\n")
+            print(f"Thanks for visiting our library. Please take care of the book you borrowed - '{bookname}'. Please return it within 30 days.\n")
             self.book.remove(bookname)
+            return True
         else:
             print("The book you are trying to borrow is not available \n")
+            return False
 
+    def returnBk(self,bookname):
+        if bookname != self.book:
+            self.book.append(bookname)
+            print("Thanks for visiting our Library. Visit again!")
+        else:
+            print("This book is not beign issued. Thanks for visiting Worlds Library")
+        
 
 class Student:
 
-    def __init__(self):
-        pass
+    def requestedbooks(self):
+        reqbook = input("Enter the name of the book you want to borrow : ")
+        return reqbook
 
-    def requestedbooks(self, bookname):
-        pass
-
-    def returnedbooks(self, bookname):
-        pass
-
+    def returnedbooks(self):
+        retnbook = input("Enter the name of the book you want to return : ")
+        return retnbook
 
 
-while(True):
+
+if __name__ == '__main__':
     myLib = Library(["python", "java", "flutter", "C",
                     "javascript", "c++", "react", "django"])
+    student = Student()
 
-    myLib.allBooks()
-    myLib.borrow("python")
-    # myLib.allBooks()
+    while(True):
+        welcomeMsg = ''' \n * * * * * WORLD LIBRARY * * * * * \n
+        Please choose an option:
+        1. List all available books
+        2. Request books
+        3. Return books 
+        4. Exit the Library \n
+        '''
+        print(welcomeMsg)
+
+        opt = int(input("Please enter the option number : "))
+
+        if opt == 1:
+            myLib.allBooks()
+        elif opt == 2:
+            myLib.borrow(student.requestedbooks())
+        elif opt == 3:
+            myLib.returnBk(student.returnedbooks())
+        elif opt == 4:
+            exit()
+        else:
+            print("Not a valid option. Please choose from 1-4. ")
